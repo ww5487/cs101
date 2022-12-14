aac56688a411e35aa5d0fab7ee83b2cfca98992d
ww5487 <120004825+ww5487@users.noreply.github.com>
2022-12-14 15:43:33 +0800

    Create main.c

---
+++ QZ#1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int a;
+	int b;
+	for(a=0; a<5; a++)
+     {
+      for(b=0; b<5; b++)
+      {
+      printf("*");
+      }
+      printf("\n");
+      }	
+	return 0;
+	
+}
